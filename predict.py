8c107cc7d9179db4a90ac8e20b88339c4d39b1ab
Luis <luis@replicate.com>
2024-06-25 21:24:23 +0000

    Updated task selection and bounding boxes

--- predict.py
+++ predict.py
@@ -1,31 +1,46 @@
 # Prediction interface for Cog ⚙️
 # https://cog.run/python
 
-from cog import BasePredictor, Input, Path
-import os
-import time
-import torch
-import subprocess
+from cog import BasePredictor, BaseModel, Input, Path
 from PIL import Image
+from typing import Optional
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from transformers import AutoProcessor, AutoModelForCausalLM 
 
 MODEL_NAME = "microsoft/Florence-2-base"
 MODEL_CACHE = "checkpoints"
-# MODEL_CACHE_URL = "https://weights.replicate.delivery/default/microsoft/Florence-2-base/model.tar"
 
-# def download_weights(url, dest):
-#     start = time.time()
-#     print("downloading url: ", url)
-#     print("downloading to: ", dest)
-#     subprocess.check_call(["pget", "-x", url, dest], close_fds=False)
-#     print("downloading took: ", time.time() - start)
+TASKS = {
+    "Caption": "<CAPTION>",
+    "Detailed Caption": "<DETAILED_CAPTION>",
+    "More Detailed Caption": "<MORE_DETAILED_CAPTION>",
+    "Caption to Phrase Grounding": "<CAPTION_TO_PHRASE_GROUNDING>",
+    "Object Detection": "<OD>",
+    "Dense Region Caption": "<DENSE_REGION_CAPTION>",
+    "Region Proposal": "<REGION_PROPOSAL>",
+    "OCR": "<OCR>",
+    "OCR with Region": "<OCR_WITH_REGION>"
+}
 
+class Output(BaseModel):
+    text: str
+    img: Optional[Path]
+
+def plot_bbox(image, data):
+    fig, ax = plt.subplots()
+    ax.imshow(image)
+    for bbox, label in zip(data['bboxes'], data['labels']):
+        x1, y1, x2, y2 = bbox
+        rect = patches.Rectangle((x1, y1), x2-x1, y2-y1, linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+        plt.text(x1, y1, label, color='white', fontsize=8, bbox=dict(facecolor='red', alpha=0.5))
+        ax.axis('off')
+    return fig
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
-        # if not os.path.exists(MODEL_CACHE):
-        #     download_weights(MODEL_CACHE_URL, MODEL_CACHE)
         self.model = AutoModelForCausalLM.from_pretrained(
             MODEL_NAME,
             trust_remote_code=True,
@@ -40,13 +55,23 @@ class Predictor(BasePredictor):
     def predict(
         self,
         image: Path = Input(description="Grayscale input image"),
-        prompt: str = Input(description="Input prompt", default="<OD>"),
-    ) -> str:
+        task_input: str = Input(
+            description="Input task",
+            choices=TASKS.keys(),
+            default="Caption"
+        ),
+        text_input: str = Input(description="Text Input(Optional)", default=None),
+    ) -> Output:
         """Run a single prediction on the model"""
         img = Image.open(image).convert("RGB")
-        inputs = self.processor(prompt, img, return_tensors="pt")
-        inputs = {key: value.to("cuda") for key, value in inputs.items()}
 
+        task = TASKS[task_input]
+        if text_input is None:
+            prompt = task
+        else:
+            prompt = task + text_input
+    
+        inputs = self.processor(prompt, img, return_tensors="pt").to("cuda")
         generated_ids = self.model.generate(
             input_ids=inputs["input_ids"],
             pixel_values=inputs["pixel_values"],
@@ -55,5 +80,13 @@ class Predictor(BasePredictor):
             num_beams=3,
         )
         generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
-        parsed_answer = self.processor.post_process_generation(generated_text, task=prompt, image_size=(img.width, img.height))
-        return str(parsed_answer)
+        parsed_answer = self.processor.post_process_generation(
+            generated_text,
+            task=task,
+            image_size=(img.width, img.height)
+        )
+        if task in ("<OD>", "<DENSE_REGION_CAPTION>", "<REGION_PROPOSAL>", "<CAPTION_TO_PHRASE_GROUNDING>"):
+            bbox_img = plot_bbox(img, parsed_answer[task])
+            bbox_img.savefig("/tmp/output.png")
+            return Output(text=str(parsed_answer), img=Path("/tmp/output.png"))
+        return Output(text=str(parsed_answer))
